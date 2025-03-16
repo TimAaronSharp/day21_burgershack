8b9ea926080816def63be0bc24d699f39c167eb3
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-15 20:40:54 -0600

    createBurger in BurgersController/Service works

--- src/controllers/BurgersController.js
+++ src/controllers/BurgersController.js
@@ -8,6 +8,7 @@ export class BurgersController extends BaseController {
     super('api/burgers')
     this.router
       .get('', this.getAllBurgers)
+      .post('', this.createBurger)
   }
   /**
    * @param {import("express").Request} req
@@ -18,6 +19,20 @@ export class BurgersController extends BaseController {
     try {
       const burgers = await burgersService.getAllBurgers()
       res.send(burgers)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").NextFunction} next
+   */
+  async createBurger(req, res, next) {
+    try {
+      const burgerData = req.body
+      const burger = await burgersService.createBurger(burgerData)
+      res.send(burger)
     } catch (error) {
       next(error)
     }
--- src/services/BurgersService.js
+++ src/services/BurgersService.js
@@ -3,11 +3,15 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class BurgersService {
+
   async getAllBurgers() {
     const burgers = await dbContext.Burger.find()
     return burgers
   }
-
+  async createBurger(burgerData) {
+    const burger = await dbContext.Burger.create(burgerData)
+    return burger
+  }
 }
 
 export const burgersService = new BurgersService()
